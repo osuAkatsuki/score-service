086db3b3f73d68f4ed17b5e0d28c321e88a41be8
tsunyoku <tsunyoku@gmail.com>
2023-10-06 17:42:17 +0100

    feat: dockerize score-service

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+if [[ -n "$KUBERNETES" ]]; then
+  source /vault/secrets/secrets.txt
+fi
+
+cd /srv/root
+
+# await connected service availability
+/scripts/await-service.sh $READ_DB_HOST $READ_DB_PORT $SERVICE_READINESS_TIMEOUT
+/scripts/await-service.sh $WRITE_DB_HOST $WRITE_DB_PORT $SERVICE_READINESS_TIMEOUT
+
+# run the service
+exec /scripts/run-service.sh
