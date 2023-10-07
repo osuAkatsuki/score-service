b40b8540dcfb11b647cbddf568f4a2e51dd0cfc3
tsunyoku <tsunyoku@gmail.com>
2023-10-08 00:59:55 +0100

    feat: add redis configuration

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -11,5 +11,9 @@ cd /srv/root
 /scripts/await-service.sh $READ_DB_HOST $READ_DB_PORT $SERVICE_READINESS_TIMEOUT
 /scripts/await-service.sh $WRITE_DB_HOST $WRITE_DB_PORT $SERVICE_READINESS_TIMEOUT
 
+# await redis availability
+/scripts/await-service.sh $REDIS_HOST $REDIS_PORT $SERVICE_READINESS_TIMEOUT
+
+
 # run the service
 exec /scripts/run-service.sh
