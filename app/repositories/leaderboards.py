92a9f9700c0404bdd4188f7fc986f368689d3efd
tsunyoku <tsunyoku@gmail.com>
2023-10-22 22:03:53 +0100

    fix: remove unneeded join

--- app/repositories/leaderboards.py
+++ app/repositories/leaderboards.py
@@ -315,7 +315,6 @@ async def fetch_beatmap_leaderboard_score_count(
     query = f"""
         WITH RankedScores AS (
             SELECT
-                c.tag,
                 users.username AS users_username,
                 s.id AS score_id,
                 s.beatmap_md5 AS beatmap_md5,
@@ -341,7 +340,6 @@ async def fetch_beatmap_leaderboard_score_count(
                 row_number() OVER (PARTITION BY s.userid ORDER BY s.{sort_column} DESC) score_order_rank
             FROM {scores_table} s
             INNER JOIN users ON users.id = s.userid
-            LEFT JOIN clans c ON users.clan_id = c.id
             INNER JOIN users_stats ON users_stats.id = s.userid
             WHERE
                 s.beatmap_md5 = :beatmap_md5
