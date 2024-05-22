c60195c47fd90ecf21c8c21c8dee669a388fbba3
cmyui <cmyuiosu@gmail.com>
2024-05-22 14:30:11 -0400

    Use embed url

--- app/models/beatmap.py
+++ app/models/beatmap.py
@@ -41,7 +41,6 @@ class Beatmap:
 
     @property
     def url(self) -> str:
-        # i hate this
         server_url = config.SRV_URL.replace("https://", "").replace("http://", "")
 
         return f"https://osu.{server_url}/beatmaps/{self.id}"
