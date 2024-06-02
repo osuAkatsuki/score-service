6cb39097884f0509fb56515b32bace9b62740e8b
cmyui <cmyuiosu@gmail.com>
2024-06-01 21:39:18 -0400

    xd

--- app/usecases/pp_cap.py
+++ app/usecases/pp_cap.py
@@ -19,8 +19,4 @@ async def get_pp_cap(mode: Mode, mods: Mods) -> int:
         f"SELECT {prefix}pp FROM pp_limits WHERE gamemode = :mode",
         {"mode": mode.as_vn},
     )
-
-    if not pp_cap:
-        return 0  # xd
-
-    return pp_cap
+    return pp_cap or 0
