dc30f134278c293aeed6664b0ed780c5b008a346
cmyui <cmyuiosu@gmail.com>
2024-08-12 02:16:47 +0200

    refmt

--- app/usecases/aggregate_score_stats.py
+++ app/usecases/aggregate_score_stats.py
@@ -6,13 +6,15 @@ import app.state.services
 async def total_scores_set() -> int:
     return sum(
         [
-            await app.state.services.database.fetch_val(
-                f"""\
+            (
+                await app.state.services.database.fetch_val(
+                    f"""\
                 SELECT count(*)
                 FROM {table}
                 """,
+                )
+                or 0
             )
-            or 0
             for table in ("scores", "scores_relax", "scores_ap")
         ],
     )
